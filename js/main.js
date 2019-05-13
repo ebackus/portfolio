249421b0390393500448f2a3f91ad3378c2dae34
unknown <ethancbackus@gmail.com>
2019-05-13 16:16:19 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var card = document.querySelector('.card');
+var card2 = document.querySelector('.card2');
+var card3 = document.querySelector('.card3');
+var card4 = document.querySelector('.card4');
+var card5 = document.querySelector('.card5');
+var card6 = document.querySelector('.card6');
+
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
